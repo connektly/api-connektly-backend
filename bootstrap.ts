6f29f2f32403864ff3579004c7cf272d2227ef18
connektly <Support@connektly.in>
2026-04-09 20:11:06 +0530

    Create bootstrap.ts

---
+++ bootstrap.ts
@@ -0,0 +1,17 @@
+const exitWithFatalError = (label: string, error: unknown) => {
+  console.error(`[startup] ${label}`);
+  console.error(error);
+  process.exit(1);
+};
+
+process.on("uncaughtException", (error) => {
+  exitWithFatalError("Uncaught exception", error);
+});
+
+process.on("unhandledRejection", (error) => {
+  exitWithFatalError("Unhandled promise rejection", error);
+});
+
+import("./server.js").catch((error) => {
+  exitWithFatalError("Server bootstrap failed", error);
+});
